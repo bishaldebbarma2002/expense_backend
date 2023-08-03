a5c27d4c45ff9c3e363c762d4a6da34c03418829
bishaldebbarma2002 <bishaldebbarmaabc123456@gmail.com>
2023-08-03 15:46:27 +0530

    first commit

---
+++ routers/expenseRoutes.js
@@ -0,0 +1,31 @@
+const { Router } = require("express");
+const {
+  add_expense,
+  view_expense,
+  view_expense_by_catergory,
+  set_budget,
+  view_expenses_in_range,
+  delete_expense,
+  get_budget,
+  get_today_expense,
+  get_expense_by_id,
+  update_expense,
+} = require("../controllers/expenseControllers");
+const { requireAuth } = require("../middleware/authMiddleware");
+const expenseRouter = Router();
+
+expenseRouter.post("/addexpense", requireAuth, add_expense);
+expenseRouter.get("/viewexpense", requireAuth, view_expense);
+expenseRouter.get("/getdailyexpense", requireAuth, get_today_expense);
+expenseRouter.post("/setbudget", requireAuth, set_budget);
+expenseRouter.get("/getbudget", requireAuth, get_budget);
+expenseRouter.get(
+  "/viewexpense/:category",
+  requireAuth,
+  view_expense_by_catergory
+);
+expenseRouter.post("/viewexpenseinrange", requireAuth, view_expenses_in_range);
+expenseRouter.get("/:id/deleteExpense", requireAuth, delete_expense);
+expenseRouter.get("/:id/getExpense", requireAuth, get_expense_by_id);
+expenseRouter.put("/:id/updateExpense", requireAuth, update_expense);
+module.exports = expenseRouter;
---
+++ server.js
@@ -0,0 +1,40 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: ".env" });
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const Router = require("./routers");
+const path = require("path");
+
+
+
+const app = express();
+
+const dbURI = process.env.DATABASE;
+const port = process.env.PORT || 8080;
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(cookieParser());
+app.use(Router);
+
+// Define a route handler for the root path ("/")
+app.get("/", (req, res) => {
+  res.send("");
+});
+
+mongoose
+  .connect(dbURI)
+  .then((result) => {
+    app.listen(port, () => {
+      console.log(`Connected to MongoDB and listening at port ${port}`);
+    });
+  })
+  .catch((err) => console.error(err));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", function (req, res) {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
